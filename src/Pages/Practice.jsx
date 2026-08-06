d529c8283965cdfed77b162137f171fb5a660ad5
Dena-Shibu <denashibu92@gmail.com>
2026-08-06 20:09:23 +0530

    Update website

--- src/Pages/Practice.jsx
+++ src/Pages/Practice.jsx
@@ -5,21 +5,21 @@ function Practice() {
     
 
     const questions=[
-        {id: 1, question: "Convert the fraction into a decimal.", fraction: "43/10", correctAnswer: "4.3"},
-        {id: 2, question: "Convert the fraction into a decimal.", fraction: "439/100", correctAnswer: "4.39"},
-        {id: 3, question: "Convert the fraction into a decimal.", fraction: "4391/1000", correctAnswer: "4.391"},
-        {id: 4, question: "Write the decimal number.", fraction: "7/10", correctAnswer: "0.7"},
-        {id: 5, question: "Write the decimal number.", fraction: "25/100", correctAnswer: "0.25"},
-        {id: 6, question: "Write the decimal number.", fraction: "9/1000", correctAnswer: "0.009"},
+        {id: 1, question: "Convert the fraction into a decimal.", fraction: "72/10", correctAnswer: "7.2"},
+        {id: 2, question: "Convert the fraction into a decimal.", fraction: "641/100", correctAnswer: "6.41"},
+        {id: 3, question: "Convert the fraction into a decimal.", fraction: "8345/1000", correctAnswer: "8.345"},
+        {id: 4, question: "Write the decimal number.", fraction: "8/10", correctAnswer: "0.8"},
+        {id: 5, question: "Write the decimal number.", fraction: "56/100", correctAnswer: "0.56"},
+        {id: 6, question: "Write the decimal number.", fraction: "5/1000", correctAnswer: "0.005"},
         {id: 7, question: "Divide and write the decimal.", fraction: "53 ÷ 10", correctAnswer: "5.3"},
         {id: 8, question: "Divide and write the decimal.", fraction: "84 ÷ 100", correctAnswer: "0.84"},
         {id: 9, question: "Divide and write the decimal.", fraction: "125 ÷ 1000", correctAnswer: "0.125"},
         {id: 10, question: "Write the fraction as a decimal.", fraction: "3/10", correctAnswer: "0.3"},
-        {id: 11, question: "Write the fraction as a decimal.", fraction: "67/100", correctAnswer: "0.67"},
-        {id: 12, question: "Identify the tenths digit.", fraction: "4.37", correctAnswer: "3"},
-        {id: 13, question: "Identify the hundredths digit.", fraction: "8.549", correctAnswer: "4"},
-        {id: 14, question: "Which number is greater?", fraction: "0.8 or 0.08", correctAnswer: "0.8"},
-        {id: 15, question: "Arrange in ascending order.", fraction: "0.5, 0.05, 0.55", correctAnswer: "0.05, 0.5, 0.55"}
+        {id: 11, question: "Write the fraction as a decimal.", fraction: "76/100", correctAnswer: "0.76"},
+        {id: 12, question: "Identify the tenths digit.", fraction: "7.34", correctAnswer: "3"},
+        {id: 13, question: "Identify the hundredths digit.", fraction: "8.999", correctAnswer: "4"},
+        {id: 14, question: "Which number is greater?", fraction: "0.1 or 0.01", correctAnswer: "0.1"},
+        {id: 15, question: "Arrange in ascending order.", fraction: "0.09, 0.9, 0.99", correctAnswer: "0.09, 0.9, 0.99"}
 
     ]
     function checkAnswer(index){
--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -9,18 +9,18 @@ const questions = [
 {id:1, question:"Convert the fraction into a decimal.", fraction:"89/10", correctAnswer:"8.9"},
 {id:2, question:"Convert the fraction into a decimal.", fraction:"909/100", correctAnswer:"9.09"},
 {id:3, question:"Convert the fraction into a decimal.", fraction:"5673/1000", correctAnswer:"5.673"},
-{id:4, question:"Write the decimal number.", fraction:"0/10", correctAnswer:"0.0"},
-{id:5, question:"Write the decimal number.", fraction:"56/100", correctAnswer:"0.56"},
-{id:6, question:"Write the decimal number.", fraction:"5/1000", correctAnswer:"0.005"},
+{id:4, question:"Write the decimal number.", fraction:"8/10", correctAnswer:"0.8"},
+{id:5, question:"Write the decimal number.", fraction:"43/100", correctAnswer:"0.43"},
+{id:6, question:"Write the decimal number.", fraction:"27/1000", correctAnswer:"0.027"},
 {id:7, question:"Divide and write the decimal.", fraction:"78 ÷ 10", correctAnswer:"7.8"},
 {id:8, question:"Divide and write the decimal.", fraction:"94 ÷ 100", correctAnswer:"0.94"},
 {id:9, question:"Divide and write the decimal.", fraction:"452 ÷ 1000", correctAnswer:"0.452"},
 {id:10, question:"Write the fraction as a decimal.", fraction:"6/10", correctAnswer:"0.6"},
-{id:11, question:"Write the fraction as a decimal.", fraction:"76/100", correctAnswer:"0.76"},
+{id:11, question:"Write the fraction as a decimal.", fraction:"66/100", correctAnswer:"0.66"},
 {id:12, question:"Identify the tenths digit.", fraction:"7.34", correctAnswer:"3"},
-{id:13, question:"Identify the hundredths digit.", fraction:"8.999", correctAnswer:"4"},
-{id:14, question:"Which number is greater?", fraction:"0.1 or 0.01", correctAnswer:"0.1"},
-{id:15, question:"Arrange in ascending order.", fraction:"0.09, 0.9, 0.99", correctAnswer:"0.09, 0.9, 0.99"}
+{id:13, question:"Identify the hundredths digit.", fraction:"0.999", correctAnswer:"4"},
+{id:14, question:"Which number is greater?", fraction:"0.9 or 0.07", correctAnswer:"0.9"},
+{id:15, question:"Arrange in ascending order.", fraction:"0.04, 0.7, 0.89", correctAnswer:"0.04, 0.7, 0.89"}
 ];
 
 function submitQuiz() {
